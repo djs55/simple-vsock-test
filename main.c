f0b1d7b9741e175d72da3692d75ccffdfec9fa10
David Scott <dave@recoil.org>
2021-01-12 10:41:18 +0000

    Add excessive debugging
    Signed-off-by: David Scott <dave@recoil.org>

--- main.c
+++ main.c
@@ -12,13 +12,17 @@ void main(){
   addr.svm_family = AF_VSOCK;
   addr.svm_port = 1025;
   addr.svm_cid = VMADDR_CID_HOST;
+  fprintf(stderr, "connecting to AF_VSOCK port 1025 on VMADDR_CID_HOST\n");
   if (connect(s, (struct sockaddr*)&addr, sizeof(addr)) != 0) {
     perror("unable to connect to the host");
   }
+  fprintf(stderr, "connected to host with fd %d\n", s);
   for (;;) {
+    fprintf(stderr, "writing '.'\n");
     if (write(s, ".", 1) != 1) {
       perror("unable to write a byte");
     }
+    fprintf(stderr, "reading one byte\n");
     char c;
     if (read(s, &c, 1) != 1) {
       perror("unable to read a byte");
